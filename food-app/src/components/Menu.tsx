9ece3aebaf1d8fa3ef9496375e19aaa518eb8a89
Andry Ariadi <andryariadi23@gmail.com>
2023-12-12 23:15:00 +0700

    update styling navbar

--- food-app/src/components/Menu.tsx
+++ food-app/src/components/Menu.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Cart from "./Cart";
 
 const links = [
-  { id: 1, title: "Homepage", url: "/" },
+  { id: 1, title: "Home", url: "/" },
   { id: 2, title: "Menu", url: "/menu" },
   { id: 3, title: "Working Hours", url: "/" },
   { id: 4, title: "Contact", url: "/" },
--- food-app/src/components/navbar.tsx
+++ food-app/src/components/navbar.tsx
@@ -1,18 +1,39 @@
 import Link from "next/link";
 import Menu from "./Menu";
 import Image from "next/image";
+import Cart from "./Cart";
 
 export default function Navbar() {
+  const user = false;
   return (
     <>
-      <nav className="h-20 flex justify-between items-center p-4 text-red-500 border-b-2 border-b-red-500">
+      <nav className="h-20 flex justify-between items-center p-4 text-rose-600 font-bold text-lg border-b-2 border-b-rose-600">
+        <div className="hidden md:flex gap-4">
+          <Link href="/" className=" hover:text-xl transition-all duration-300">
+            Home
+          </Link>
+          <Link href="/menu" className=" hover:text-xl transition-all duration-300">
+            Menu
+          </Link>
+          <Link href="/contact" className=" hover:text-xl transition-all duration-300">
+            Contact
+          </Link>
+        </div>
         <div className="text-2xl font-bold">
           <Link href="/">
             <Image src="/mcfood.svg" alt="logo" width={180} height={180} />
           </Link>
         </div>
-        <div>
+        <div className="md:hidden">
           <Menu />
+        </div>
+        <div className="hidden md:flex gap-4 items-center">
+          <div className="flex items-center gap-2 bg-orange-300 p-1 rounded-md cursor-pointer hover:text-xl transition-all duration-300">
+            <Image src="/phone.png" width={20} height={20} alt="phone" />
+            <p>+62 123 456</p>
+          </div>
+          <div className="hover:text-xl transition-all duration-300">{user ? <Link href="/orders">Order</Link> : <Link href="/login">Login</Link>}</div>
+          <Cart />
         </div>
       </nav>
     </>
