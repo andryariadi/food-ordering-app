8e194f5a8dc64de043722820e8c7f778c0629f80
Andry Ariadi <andryariadi23@gmail.com>
2023-12-12 23:19:54 +0700

    styling footer

--- food-app/src/components/footer.tsx
+++ food-app/src/components/footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>&copy; 2022 Food App. All rights reserved.</p>
+    <footer className="text-center font-semibold text-rose-600">
+      <Link href="/">Andry Ariadi &copy; 2023 Food App. All rights reserved.</Link>
     </footer>
   );
 }
