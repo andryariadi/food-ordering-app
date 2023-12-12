af19f44b64a33b29847009e910fc0e5b4efe44b1
Andry Ariadi <andryariadi23@gmail.com>
2023-12-12 16:53:29 +0700

    update styling navbar

--- food-app/src/components/Menu.tsx
+++ food-app/src/components/Menu.tsx
@@ -26,7 +26,7 @@ export default function Menu() {
         <Image src={isOpen ? "/open.png" : "/close.png"} alt="menu" width={20} height={20} onClick={toggleMenu} />
       </div>
       {!isOpen && (
-        <div className="bg-red-500 text-white absolute left-0 top-24 w-screen h-[calc(100vh-6rem)] flex justify-center items-center flex-col gap-8 text-2xl font-semibold ">
+        <div className="bg-red-500 text-white absolute left-0 top-24 w-screen h-[calc(100vh-6rem)] flex justify-center items-center flex-col gap-8 text-2xl font-semibold" onClick={toggleMenu}>
           {links.map((link) => (
             <Link key={link.id} href={link.url} className="hover:text-3xl transition-all duration-500">
               {link.title}
