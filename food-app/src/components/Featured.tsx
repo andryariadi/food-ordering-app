47cd08bbd6ab8bdc3a544954e79a20d4838b84c1
Andry Ariadi <andryariadi23@gmail.com>
2023-12-13 05:53:10 +0700

    styling featured page

--- food-app/src/components/Featured.tsx
+++ food-app/src/components/Featured.tsx
@@ -1,7 +1,32 @@
+import { featuredProducts } from "@/data";
+import Image from "next/image";
+
 export default function Featured() {
   return (
     <>
-      <h1>Featured</h1>
+      <div className=" w-screen overflow-x-scroll text-rose-600">
+        {/* WRAPPER */}
+        <div className="w-max flex">
+          {/* CARD */}
+          {featuredProducts.map((product) => (
+            <div key={product.id} className="bg-teal-500 w-screen md:w-[50vw] xl:w-[33vw] h-[60vh] xl:h-[90vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300">
+              {product.img && (
+                <div className="bg-red-500 relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+                  <Image src={product.img} alt="featured" fill className="object-contain" />
+                </div>
+              )}
+              <div className="bg-orange-500 flex-1 flex flex-col items-center justify-center text-center gap-4 py-5">
+                <h1 className="text-xl xl:text-2xl 2xl:text-3xl font-bold uppercase">{product.title}</h1>
+                <p className="p-4 2xl:p-8">{product.desc}</p>
+                <span className="text-xl font-bold">${product.price}</span>
+                <button className="bg-rose-600 text-white p-2 rounded-md">Add to Cart</button>
+              </div>
+            </div>
+          ))}
+          {/* END CARD */}
+        </div>
+        {/* END WRAPPER */}
+      </div>
     </>
   );
 }
