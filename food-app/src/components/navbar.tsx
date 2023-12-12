84709d9f6b37c79147300f40fa59bda8bd773051
Andry Ariadi <andryariadi23@gmail.com>
2023-12-12 13:09:08 +0700

    add navbar comp

---
+++ food-app/src/components/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>Food App</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Recipe</a>
+      </div>
+    </nav>
+  );
+}
