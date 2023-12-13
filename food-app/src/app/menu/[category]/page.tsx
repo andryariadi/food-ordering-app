ee47e133efc6146adda2fe544454010392069621
Andry Ariadi <andryariadi23@gmail.com>
2023-12-14 06:04:16 +0700

    styling menu page

--- food-app/src/app/menu/[category]/page.tsx
+++ food-app/src/app/menu/[category]/page.tsx
@@ -1,8 +1,25 @@
+import { pizzas } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function CategoryPage() {
   return (
     <>
-      <main>
-        <h1>Category Page</h1>
+      <main className="flex flex-wrap">
+        {pizzas.map((item) => (
+          <Link href={`/product/${item.id}`} key={item.id} className="w-full sm:w-1/2 lg:w-1/3 h-[60vh] border-r-2 border-b-2 border-rose-600 p-4 flex flex-col justify-between group even:bg-fuchsia-50">
+            {item.img && (
+              <div className="relative h-[80%]">
+                <Image src={item.img} alt="pizza" fill className="object-contain" />
+              </div>
+            )}
+            <div className="flex items-center justify-between font-bold text-rose-600">
+              <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+              <p className="text-xl group-hover:hidden">${item.price}</p>
+              <button className="hidden bg-rose-600 text-white p-2 rounded-md group-hover:block">Add to Cart</button>
+            </div>
+          </Link>
+        ))}
       </main>
     </>
   );
--- food-app/src/app/menu/page.tsx
+++ food-app/src/app/menu/page.tsx
@@ -6,10 +6,10 @@ export default function MenuPage() {
     <>
       <main className="p-4 md:px-20 xl:px-40 h-[calc(100vh-7rem)] flex flex-col md:flex-row items-center">
         {menu.map((item) => (
-          <Link href={`/menu/${item.slug}`} key={item.id} style={{ backgroundImage: `url(${item.img})` }} className="w-full h-1/2 md:h-3/5 bg-cover bg-center p-8">
+          <Link href={`/menu/${item.slug}`} key={item.id} style={{ backgroundImage: `url(${item.img})` }} className="w-full h-1/2 md:h-5/6 bg-cover bg-center p-8">
             <div className={`text-${item.color} w-1/2 h-full justify-evenly flex flex-col`}>
-              <h1 className="text-3xl font-bold uppercase">{item.title}</h1>
-              <p className="text-sm my-8">{item.desc}</p>
+              <h1 className="text-2xl md:text-3xl font-bold uppercase">{item.title}</h1>
+              <p className="text-sm my-3">{item.desc}</p>
               <button className={`hidden 2xl:block py-2 px-4 w-1/2 rounded-md bg-amber-500 text-white`}>Explore</button>
             </div>
           </Link>
