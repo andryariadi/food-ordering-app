7df0692c669777598b05f31d3556f35afed75f2f
Andry Ariadi <andryariadi23@gmail.com>
2023-12-14 14:33:56 +0700

    styling cart page

--- food-app/src/app/cart/page.tsx
+++ food-app/src/app/cart/page.tsx
@@ -1,8 +1,58 @@
+import Image from "next/image";
+
 export default function CartPage() {
   return (
     <>
-      <main>
-        <h1>Cart Page</h1>
+      <main className="flex flex-col lg:flex-row h-screen justify-center items-center text-rose-600">
+        <div className="container-cart flex-1 h-full w-full p-4 flex flex-col md:flex-row justify-center flex-wrap overflow-y-scroll lg:p-20 xl:p-30">
+          <div className="w-full flex items-center justify-between mb-2">
+            <Image src="/temporary/p1.png" alt="pizza" width={100} height={100} />
+            <div>
+              <h1 className="text-2xl font-bold uppercase">Silician</h1>
+              <span>Large</span>
+            </div>
+            <h2 className="font-bold">$24.90</h2>
+            <span className="cursor-pointer">X</span>
+          </div>
+          <div className="w-full flex items-center justify-between mb-2">
+            <Image src="/temporary/p1.png" alt="pizza" width={100} height={100} />
+            <div>
+              <h1 className="text-2xl font-bold uppercase">Silician</h1>
+              <span>Large</span>
+            </div>
+            <h2 className="font-bold">$24.90</h2>
+            <span className="cursor-pointer">X</span>
+          </div>
+          <div className="w-full flex items-center justify-between mb-2">
+            <Image src="/temporary/p1.png" alt="pizza" width={100} height={100} />
+            <div>
+              <h1 className="text-2xl font-bold uppercase">Silician</h1>
+              <span>Large</span>
+            </div>
+            <h2 className="font-bold">$24.90</h2>
+            <span className="cursor-pointer">X</span>
+          </div>
+        </div>
+        <div className="bg-fuchsia-50 flex-1 h-full w-full p-4 flex flex-col gap-4 justify-center lg:p-20 xl:p-30 2xl:text-xl 2xl:gap-6">
+          <div className="flex justify-between">
+            <span>Subtotal (3 items)</span>
+            <span>$74.70</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Service Cost</span>
+            <span>$00.0</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Delivery Cost</span>
+            <span className="text-teal-600">FREE!</span>
+          </div>
+          <hr className="border-rose-600 my-2" />
+          <div className="flex justify-between">
+            <span>TOTAL (INCL. VAT)</span>
+            <span className="font-bold">$74.70</span>
+          </div>
+          <button className="bg-rose-600 text-white p-2 rounded-md self-end">CheckOut</button>
+        </div>
       </main>
     </>
   );
--- food-app/src/app/product/[id]/page.tsx
+++ food-app/src/app/product/[id]/page.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 
 export default function DetailProduct() {
   return (
-    <section className="h-screen flex flex-col md:flex-row p-4 lg:px-20 xl:px-40 items-center">
+    <section className="h-screen flex flex-col md:flex-row gap-5 p-4 lg:px-20 xl:px-40 items-center mb-5">
       {singleProduct.img && (
         <div className="relative w-full h-fit ">
           <Image src={singleProduct.img} alt="pizza" width={500} height={500} className="object-contain" />
         </div>
       )}
       <div className="h-fit flex flex-col gap-4 text-rose-600">
-        <h1 className="text-3xl font-bold uppercase">{singleProduct.title}</h1>
+        <h1 className="text-3xl xl:text-5xl font-bold uppercase">{singleProduct.title}</h1>
         <p>{singleProduct.desc}</p>
         <Price id={singleProduct.id} price={singleProduct.price} options={singleProduct.options} />
       </div>
