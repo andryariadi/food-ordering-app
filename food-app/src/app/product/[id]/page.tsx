48a1d41c91c09a070dfd491f6c137881043e275d
Andry Ariadi <andryariadi23@gmail.com>
2023-12-14 07:00:52 +0700

    styling product

--- food-app/src/app/product/[id]/page.tsx
+++ food-app/src/app/product/[id]/page.tsx
@@ -1,20 +1,20 @@
+import Price from "@/components/Price";
 import { singleProduct } from "@/data";
 import Image from "next/image";
 
 export default function DetailProduct() {
   return (
-    <>
-      <main className="h-screen flex flex-col md:flex-row p-4 lg:px-20 xl:px-40">
-        {singleProduct.img && (
-          <div className="relative">
-            <Image src={singleProduct.img} alt="pizza" width={500} height={500} className="object-contain" />
-          </div>
-        )}
-        <div>
-          <h1>{singleProduct.title}</h1>
-          <p>{singleProduct.desc}</p>
+    <section className="h-screen flex flex-col md:flex-row p-4 lg:px-20 xl:px-40 items-center">
+      {singleProduct.img && (
+        <div className="relative w-full h-fit ">
+          <Image src={singleProduct.img} alt="pizza" width={500} height={500} className="object-contain" />
         </div>
-      </main>
-    </>
+      )}
+      <div className="h-fit flex flex-col gap-4 text-rose-600">
+        <h1 className="text-3xl font-bold uppercase">{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+        <Price id={singleProduct.id} price={singleProduct.price} options={singleProduct.options} />
+      </div>
+    </section>
   );
 }
--- food-app/src/components/Price.tsx
+++ food-app/src/components/Price.tsx
@@ -1,3 +1,33 @@
-export default function Price({ price }: { price: number }) {
-  return <div className="text-amber-500 text-3xl font-bold">${price.toFixed(2)}</div>;
+type Props = {
+  id: number;
+  price: number;
+  options?: { title: string; additionalPrice: number }[];
+};
+
+export default function Price({ id, price, options }: Props) {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <h2 className="text-2xl font-bold">${price.toFixed(2)}</h2>
+        <div className="flex gap-4">
+          {options?.map((option) => (
+            <button key={option.title} className="p-2 ring-1 ring-rose-600 rounded-md">
+              {option.title}
+            </button>
+          ))}
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="w-full flex justify-between p-3 ring-1 ring-rose-600">
+            <span>Quantity</span>
+            <div className="flex gap-4 items-center">
+              <button>{"<"}</button>
+              <span>1</span>
+              <button>{">"}</button>
+            </div>
+          </div>
+          <button className="bg-rose-600 text-white p-[0.80rem] w-56">Add to Cart</button>
+        </div>
+      </div>
+    </>
+  );
 }
