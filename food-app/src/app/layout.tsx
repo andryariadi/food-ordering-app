d862e7670454b2b193a83ec9c062219dc6672f01
Andry Ariadi <andryariadi23@gmail.com>
2023-12-17 21:50:37 +0700

    update layout

--- food-app/src/app/layout.tsx
+++ food-app/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Notifications from "@/components/Notification";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Notifications from "../components/Notification";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <>
-          <Notifications />
-          <Navbar />
-          {children}
-          <Footer />
-        </>
+        <Notifications />
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
