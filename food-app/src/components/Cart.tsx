d6c0c7be4f69a07e11b1fff15132600c86f965a5
Andry Ariadi <andryariadi23@gmail.com>
2023-12-17 21:34:42 +0700

    update cart

--- food-app/src/components/Cart.tsx
+++ food-app/src/components/Cart.tsx
@@ -5,9 +5,6 @@ import { FaShoppingCart } from "react-icons/fa";
 export default function Cart() {
   return (
     <Link href="/cart" className="flex gap-2 items-center">
-      <div className="relative w-8 h-8">
-        <Image src="/cart.png" alt="cart" fill />
-      </div>
       <div className="relative flex items-center">
         <div className="flex items-center justify-center absolute right-[-5px] top-[0] text-[10px] bg-amber-400 h-5 w-5 rounded-full text-white">
           <span>5</span>
