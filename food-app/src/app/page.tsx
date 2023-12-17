9dc499ea8a71b601dcefc9e232ba1624e4c568b8
Andry Ariadi <andryariadi23@gmail.com>
2023-12-17 22:43:03 +0700

    update last

--- food-app/src/app/page.tsx
+++ food-app/src/app/page.tsx
@@ -1,7 +1,4 @@
 import Featured from "@/components/Featured";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import Notifications from "@/components/Notification";
 import Offer from "@/components/Offer";
 import Slider from "@/components/Slider";
 
@@ -9,12 +6,9 @@ export default function Home() {
   return (
     <>
       <main>
-        <Notifications />
-        <Navbar />
         <Slider />
         <Featured />
         <Offer />
-        <Footer />
       </main>
     </>
   );
