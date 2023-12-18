61e146d6f1abcfb108a4c1374e3cd8af1377706d
Andry Ariadi <andryariadi23@gmail.com>
2023-12-18 14:26:57 +0700

    update lagi coy

--- food-app/src/components/notification.tsx
+++ food-app/src/components/notification.tsx
@@ -2,7 +2,7 @@ import React from "react";
 const Notification = () => {
   return (
     <>
-      <div className="h-12 bg-rose-600 text-amber-400 text-center text-sm md:text-base cursor-pointer px-4 flex justify-center items-center">Free delivery for all orders over $50. Order your food now!</div>
+      <div className="h-12 bg-rose-600 text-amber-400 text-center text-sm md:text-base cursor-pointer px-4 flex justify-center items-center">Free delivery for all orders over $50. Order your food now !</div>
     </>
   );
 };
